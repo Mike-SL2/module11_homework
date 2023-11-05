01e3a58290ee3f3b28134f592aca81f0e9fff19d
Mike-SL2 <147996075+Mike-SL2@users.noreply.github.com>
2023-11-05 13:37:09 +0700

    Add files via upload

---
+++ js-tasks/js_task5.js
@@ -0,0 +1,12 @@
+﻿//Задание 5
+//Напишите функцию, которая принимает два натуральных числа x и n и возвращает x в степени n. 
+//Иначе говоря, умножает x на себя n раз и возвращает результат.
+//Используйте Arrow Function синтаксис.
+//Протестируйте функцию на любых значениях и выведите результат в консоль.
+//-------------------------------------------------------------------------
+const pwr = (X,N)=>{console.log(X,'в степени',N);let R=1;while (N) {R=R*X;N--;} return R;}	
+
+console.log(pwr(3,3));	console.log(pwr(2,0));	console.log(pwr(5,3));	console.log(pwr(5,2));
+console.log(pwr(2,10));	console.log(pwr(2,1));	console.log(pwr(2,8));	console.log(pwr(10,6));
+
+console.log(pwr(4,3));	console.log(pwr(256,2));console.log(pwr(9,3));	console.log(pwr(64,2));
